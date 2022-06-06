a78527e2bddef7865616366bd3767449f6b96844
Evan Garcia <evanag116@yahoo.com>
2022-06-06 13:21:42 -0500

    completed in py

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,20 @@
-def translate(word_or_phrase):
-  pass
+def translate(str):
+  words = str.split()
+  capitalized = [i[0].isupper() for i in words]
+  vowels = "aeiou"
 
+  for j in range(len(words)):
+    words[j] = words[j].lower()
+    for letter in words[j]:
+      if letter in vowels:
+        break
+      else:
+        words[j] = words[j][1:] + words[j][0]
+    words[j] += "ay"
+    if capitalized[j]:
+      words[j] = words[j].title()
+
+  return " ".join(words)
+
+
+print(translate("the quick brown fox"))
